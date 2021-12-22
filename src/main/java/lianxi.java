d7c3b8a5460ae44e08e85a947a20c2cf11e278ed
hongheng23 <1653756831@qq.com>
2021-12-22 09:29:56 +0800

    111222

--- src/main/java/lianxi.java
+++ src/main/java/lianxi.java
@@ -1,6 +1,6 @@
 public class lianxi {
     public static void main(String[] args) {
-        System.out.println("1111");
-        System.out.println("2222");
+        System.out.println("111");
+        System.out.println("222");
     }
 }
