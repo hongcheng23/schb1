d3e548213c208f7e10c7daa8aa4d5cc2601e15af
hongcheng23 <96402451+hongcheng23@users.noreply.github.com>
2021-12-22 14:09:05 +0800

    Update lianxi.java

--- src/main/java/lianxi.java
+++ src/main/java/lianxi.java
@@ -1,6 +1,6 @@
 public class lianxi {
     public static void main(String[] args) {
         System.out.println("111");
-        System.out.println("222");
+        System.out.println("2223");
     }
 }
